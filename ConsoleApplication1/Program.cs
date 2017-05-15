e74a2d9da7a52a281e7867d62afbb2c5d929fef5
jiaokang <Administrator@HP201-1>
2017-05-15 19:55:40 +0800

    完成

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -139,13 +139,129 @@ namespace ConsoleApplication1
                    }
                } while (i <= 100);*/     //有错误
 
-         /*   for (int i = 0; i < 100; i++)
+            /*   for (int i = 0; i < 100; i++)
+               {
+                   if (i % 3 == 0) {
+                       Console.WriteLine(i);
+                   }
+               }*/
+            #endregion
+
+            #region 6.编写一个程序,do-while语句,输入5个数,求和
+            /*  int i=0;
+              int sum1;
+              int res=0;
+              do
+              {
+                  Console.Write("请输入第{0}个数:",i+1);
+                   sum1 = Convert.ToInt32(Console.ReadLine());
+                  res = res + sum1;
+                  i++;
+              }while(i<5);
+              Console.WriteLine("这5个整数的和是:{0}",res);*/
+            #endregion
+
+            #region 7.编写一个程序,while语句求出1+(1+2)+(1+2+3)...之和
+            /* int k = 10;
+             int m = 0;
+             for (int i = 1; i <= k; i++)
+             {
+                 int n = 0;
+                 for (int j = 1; j <= i; j++) {
+                     n += j;
+                 }
+                 m += n;
+             }
+             Console.WriteLine(m);*/
+            #endregion
+
+            #region 8.while登录
+            /*  int i = 0;
+              const string userName = "admin";
+              const string passWord = "123456";
+              while (i < 3)
+              {
+                  Console.WriteLine("请输入用户名:");
+                  string user = Console.ReadLine();
+                  Console.WriteLine("请输入密码:");
+                  string pass = Console.ReadLine();
+                  if ((user == userName) && (pass == passWord))
+                  {
+                      Console.WriteLine("登陆成功");
+                      break;
+                  }
+                  else {
+                      Console.WriteLine("登录失败,重新输入");
+                  }
+                  i++;
+              }*/
+            #endregion
+
+            #region  9.for循环100以内偶数和,奇数和分别多少
+            /*   int sum1 = 0;
+               int sum2 = 0;
+               for (int i = 1; i <= 100; i++)
+               {
+                   if (i % 2 == 0)
+                   {
+                       sum1 += i;
+                   }
+                   else {
+                       sum2 += i;
+                   }
+               }
+               Console.WriteLine("偶数和是:{0},奇数和是:{1}",sum1,sum2);*/
+
+
+            #endregion
+
+            #region 10.水仙花数
+            /*  int temp = 0;
+              for (int i = 100; i < 999; i++)
+              {
+                  temp = i;
+                  int x = temp % 10;
+                  temp = temp / 10;
+                  int y = temp % 10;
+                  int z = temp / 10;
+                  if (i == x * x * x + y * y * y + z * z * z)
+                  {
+                      Console.WriteLine(i);
+                  }
+              }*/
+            #endregion
+
+            #region 11.小球下落
+            /*double s = 0;
+            double t = 100;
+            for (int i = 1; i <= 10; i++)
             {
-                if (i % 3 == 0) {
-                    Console.WriteLine(i);
+                s = s+t;
+                t = t / 2;
+            }
+            Console.WriteLine(s);
+            Console.WriteLine(t);*/
+            #endregion
+
+            #region 12.猴子吃桃子
+            //
+            #endregion
+
+            #region 13.圣诞树
+           /* for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j <= 6 - i; j++)
+                {
+                    Console.Write(" ");
                 }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine( );
             }*/
             #endregion
+
         }
     }
 }
